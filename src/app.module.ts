bf8e926383ff6ed2190e37038a30c64cffd4ea09
Matheus Spirlandeli Batista <matsb97@hotmail.com>
2022-02-26 12:36:55 -0300

    Adicionando Swagger na API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import {InterestPointsModule } from './interestPoints/interestPoints.module';
 @Module({
   imports: [InterestPointsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
