7f6429a5945bb6efe69782ee51b32678cc5448bf
Matheus Spirlandeli Batista <matsb97@hotmail.com>
2022-02-26 15:07:37 -0300

    Criação de endpoint de cadastro de pontos de interesse

--- src/interestPoints/interestPoints.controller.ts
+++ src/interestPoints/interestPoints.controller.ts
@@ -1,18 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { InterestPointsService } from './interestPoints.service';
-import {InterestPoints} from './interestPoints.entity'
-import { ApiTags, ApiResponse, ApiOperation, ApiParam, ApiQuery } from '@nestjs/swagger';
+import { InterestPoints } from './interestPoints.entity'
+import { ApiTags, ApiResponse, ApiOperation, ApiParam, ApiQuery,ApiBody } from '@nestjs/swagger';
+import { InterestPointsCreateDto } from './dto/interestPoint.create.dto';
+import { ResponseDto } from 'src/dto/response.dto';
 @Controller('interestPoints')
 @ApiTags('Configuração de Pontos de Interesse')
 export class InterestPointsController {
-  constructor(private readonly interestPointsService: InterestPointsService) {}
+  constructor(private readonly interestPointsService: InterestPointsService) { }
 
   @Get()
   @ApiOperation({ summary: 'Listar todos Pontos de Interesse' })
-  @ApiResponse({ status: 200, description: 'Listagem dos Pontos de Interesse realizadado com sucesso.'})
+  @ApiResponse({ status: 200, description: 'Listagem dos Pontos de Interesse realizadado com sucesso.' })
   @ApiResponse({ status: 404, description: 'Registro não encontrado' })
   @ApiResponse({ status: 500, description: 'Erro na tentativa de buscar o registro' })
   async findAll(): Promise<InterestPoints[]> {
     return this.interestPointsService.findAll();
   }
+
+  @Post('/cadastrar')
+  @ApiBody({ type: InterestPointsCreateDto })
+  @ApiOperation({ summary: 'Cadastrar novos Pontos de Interesse' })
+  @ApiResponse({ status: 201, description: 'Registro inserido com sucesso.' })
+  @ApiResponse({ status: 500, description: 'Erro na tentativa de inserir o registro' })
+  async create(@Body() data: InterestPointsCreateDto): Promise<ResponseDto> {
+    return this.interestPointsService.create(data);
+  }
+
 }
--- src/interestPoints/interestPoints.service.ts
+++ src/interestPoints/interestPoints.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, Inject } from '@nestjs/common';
+import { ResponseDto } from 'src/dto/response.dto';
 import { Repository } from 'typeorm';
+import { InterestPointsCreateDto } from './dto/interestPoint.create.dto';
 import { InterestPoints } from './interestPoints.entity';
 
 @Injectable()
@@ -7,9 +9,46 @@ export class InterestPointsService {
   constructor(
     @Inject('INTEREST_POINTS_REPOSITORY')
     private interestPointsRepository: Repository<InterestPoints>,
-  ) {}
+  ) { }
 
   async findAll(): Promise<InterestPoints[]> {
     return this.interestPointsRepository.find();
   }
+
+  async create(data: InterestPointsCreateDto) {
+    let interestPoint = new InterestPoints();
+
+    interestPoint.name = data.name;
+    interestPoint.open = data.open;
+    interestPoint.close = data.close;
+    interestPoint.latitude = data.latitude;
+    interestPoint.longitude = data.longitude;
+
+    if (interestPoint.latitude < 0) {
+      return <ResponseDto>{
+        status: false,
+        message: "Valor de Latitude(X) deve ser inteiro e maior que 0!"
+      }
+    }
+    if (interestPoint.longitude < 0) {
+      return <ResponseDto>{
+        status: false,
+        message: "Valor de Longitude(Y) deve ser inteiro e maior que 0!"
+      }
+    }
+
+    return await this.interestPointsRepository.save(interestPoint).then(async response => {
+      return <ResponseDto>{
+        status: true,
+        message: "Ponto de interesse cadastrado com sucesso!"
+      }
+    }).catch(error => {
+      return <ResponseDto>{
+        status: false,
+        message: "Houve algum erro para cadastrar ponto de interesse!"
+      }
+    })
+
+  }
+
 }
